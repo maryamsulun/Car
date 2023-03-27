428c9bbb37d6da342e3f9dfd44eaa77f7c0bc95e
maryamsulun <125920102+maryamsulun@users.noreply.github.com>
2023-03-27 15:14:45 +0200

    test

---
+++ src/Car.java
@@ -0,0 +1,78 @@
+import java.util.Objects;
+public class Car {
+    private String model;
+    private String brand;
+    private int year;
+    private int price;
+    private String color;
+    private int quantity;
+
+    public Car(String model, String brand, int year, int price, String color, int quantity) {
+        this.model = model;
+        this.brand = brand;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.quantity = quantity;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public String getColor(){
+        return color;}
+    public void setColor(String color){
+        this.color = color;}
+    public int getPrice() {
+        return price;}
+    public void setPrice(int price) {
+        this.price = price;}
+    public int getQuantity() {
+        return quantity;}
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;}
+
+@Override
+    public String toString () {
+            return "Car{" +
+                    "model='" + model + '\'' +
+                    ", brand ='" + brand + '\'' +
+                    ", year =" + year +
+                    ", price =" + price +
+                    ", color =" + color +
+                    ", quantity =" + quantity +
+                    '}';
+        }
+@Override
+        public boolean equals (Object o){
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Car car = (Car) o;
+            return year == car.year && price == car.price
+                    && quantity == car.quantity && Objects.equals(model, car.model) && Objects.equals(brand,car.brand) && Objects.equals(color,car.color);
+        }
+@Override
+        public int hashCode () {
+            return Objects.hash(model, brand, year, price, color, quantity);
+        }
+        void sell ( int sold){
+            quantity -= sold;
+        }
+        void delivery ( int delivery){
+            quantity += delivery;
+        }
+}
